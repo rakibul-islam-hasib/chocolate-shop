2910add4d864d172f8f65d441419b35041e25122
Hasib Hossain <boraborhasib@gmail.com>
2023-05-09 13:15:56 +0600

    DATA POST method added

--- Client/src/components/AddChocolate.jsx
+++ Client/src/components/AddChocolate.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import DropDown from '../utilities/DropDown';
-
+import Swal from 'sweetalert2'
 const AddChocolate = () => {
     const [category, setCategory] = useState(''); 
     const handelFormSubmit = e => {
@@ -10,7 +9,26 @@ const AddChocolate = () => {
         const country = e.target.country.value ;
         const photo = e.target.photo.value ;
         const chocolate = {name , country , photo , category}
-        console.log(chocolate)
+        fetch('http://localhost:5000/chocolates' , {
+            method : 'POST' , 
+            headers : { 
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(chocolate)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.insertedId) {
+                Swal.fire(
+                    'Good job!',
+                    'Chocolate added to database!',
+                    'success'
+                  )
+                  e.target.reset(); 
+            }
+        })
+        // console.log(chocolate)
     }
     const dropdown = (value) => {
         // console.log(value)
@@ -22,15 +40,15 @@ const AddChocolate = () => {
             <form onSubmit={handelFormSubmit}>
                 <div className="">
                     <label htmlFor="name" className='block ml-2 mb-2 uppercase text-base font-bold'>Name</label>
-                    <input type="text" name='name' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Chocolate name' />
+                    <input required type="text" name='name' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Chocolate name' />
                 </div>
                 <div className="">
                     <label htmlFor="name" className='block ml-2 mb-2 uppercase text-base mt-3 font-bold'>Country/Factory</label>
-                    <input type="text" name='country' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Country/Factory name' />
+                    <input required type="text" name='country' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Country/Factory name' />
                 </div>
                 <div className="my-3">
                     <label htmlFor="name" className='block ml-2 mb-2 uppercase text-base font-bold'>Image URL</label>
-                    <input type="text" name='photo' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Image URL' />
+                    <input required type="text" name='photo' className='w-full py-3 px-7 text-base outline-none rounded-lg' placeholder='Image URL' />
                 </div>
                 <div className="w-full">
                 <label htmlFor="" className='block ml-2 my-2 uppercase text-base font-bold'>Category</label>
