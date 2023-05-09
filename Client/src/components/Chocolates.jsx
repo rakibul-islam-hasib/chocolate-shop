0e414158b11d97acd6b0f189c24c7e85e912d66b
Hasib Hossain <boraborhasib@gmail.com>
2023-05-09 14:30:50 +0600

    Show all chocolate part done

--- Client/src/components/Chocolates.jsx
+++ Client/src/components/Chocolates.jsx
@@ -4,7 +4,10 @@ import { MdDeleteSweep } from 'react-icons/md'
 import { useLoaderData } from 'react-router-dom';
 
 const Chocolates = () => {
-    const allChocolate = useLoaderData(); 
+    const allChocolate = useLoaderData();
+    console.log(allChocolate)
+
+
     return (
         <>
             <div className=" rounded-lg overflow-hidden mt-3">
@@ -19,23 +22,23 @@ const Chocolates = () => {
                         </tr>
                     </thead>
                     <tbody className="bg-white">
-                        <tr>
-                            <td className="py-4 w-full px-6 border-b border-gray-200">
-                                <img src="https://i.ibb.co/hdhwgJt/5.png" className='h-[70px]' alt="" />
-                            </td>
-                            <td className="py-4 w-full px-6 border-b border-gray-200 truncate">johndoe@gmail.com</td>
-                            <td className="py-4 w-full px-6 border-b border-gray-200">555-555-5555</td>
-                            <td className="py-4 w-full px-6 border-b border-gray-200">555-555-5555</td>
-                            <td className="py-4 w-full px-6 border-b border-gray-200">
-                                {/* <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span> */}
-                                <div className="flex items-center gap-2">
-                                    <span className='text-2xl p-2 rounded-lg bg-orange-200'><FiEdit /> </span>
-                                    <span className='text-2xl p-2 rounded-lg  bg-orange-200'><MdDeleteSweep /> </span>
-                                </div>
-                            </td>
-                        </tr>
-
-
+                        {
+                            allChocolate.map(item => <tr key={item._id}>
+                                <td className="py-4 w-full px-6 border-b border-gray-200">
+                                    <img src={item.photo} className='h-[70px]' alt="" />
+                                </td>
+                                <td className="py-4 w-full px-6 border-b border-gray-200 truncate">{item.name}</td>
+                                <td className="py-4 w-full px-6 border-b border-gray-200">{item.country}</td>
+                                <td className="py-4 w-full px-6 border-b border-gray-200">{item.category ? item.category : 'Not Provided'}</td>
+                                <td className="py-4 w-full px-6 border-b border-gray-200">
+                                    {/* <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span> */}
+                                    <div className="flex items-center gap-2">
+                                        <span className='text-2xl p-2 rounded-lg bg-orange-200'><FiEdit /> </span>
+                                        <span className='text-2xl p-2 rounded-lg  bg-orange-200'><MdDeleteSweep /> </span>
+                                    </div>
+                                </td>
+                            </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
